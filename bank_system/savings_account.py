08f730a307ccae05077639f28a938593e4e96498
Winstar <james.canoy87@gmail.com>
2025-06-28 14:23:48 +0800

    Implement deposit, withdraw, and get_balance methods for SavingsAccount

--- bank_system/savings_account.py
+++ bank_system/savings_account.py
@@ -1,5 +1,24 @@
+from account import Account
+
 class SavingsAccount(Account):
     def __init__(self, account_number, account_holder, pin, balance=0.0):
         super().__init__(self, account_holder, account_number, pin)
         self._balance = balance
         self._role = "Customer"
+
+    def deposit(self,amount):
+        if amount < 0:
+            raise ValueError("Deposited amount must be positive")
+        self._balance += amount
+        self.add_transactions(f"Deposited: P{amount:.2f}")
+        return self._balance
+    
+    def withdraw(self, amount):
+        if amount < 0:
+            raise ValueError("Withdrawal amount must be positive")
+        if amount > self._balance:
+            raise ValueError("Insufficient Funds")
+        
+    def get_balance(self):
+        return self._balance
+    
