2544af2860ca070623cf8e71becabb2fbbd000c5
Winstar <james.canoy87@gmail.com>
2025-06-28 13:38:06 +0800

    Added Account class as an abstract base using abc module

---
+++ bank_system/account.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+class Account(ABC):
+    def __init__(self, account_number, account_holder, pin, balance=0.0):
+        self._account_number = account_number
+        self._account_holder = account_holder
+        self._pin = pin
+        self._balance = balance
+        self._transactions = []
