afccc36a9e1dc0b28b56fcd06358f1700a0da09f
Winstar <james.canoy87@gmail.com>
2025-06-28 14:12:11 +0800

    Implemented core methods: verify_pin(), get_account_info(), and transaction logging

--- bank_system/account.py
+++ bank_system/account.py
@@ -15,3 +15,18 @@ class Account(ABC):
     @abstractmethod
     def withdraw(self, amount):
        pass
+
+    def verify_pin(self, pin):
+        return self._pin == pin
+
+    def get_account_info(self):
+        return {
+            "Account Number" : self._account_number,
+            "Account Holder" : self._account_holder
+        }
+
+    def add_transactions(self, detail):
+        return self._transactions.append(detail)
+    
+    def get_transactions(self):
+        return self._transactions
